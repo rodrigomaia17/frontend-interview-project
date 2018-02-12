2eb8a1332ec94fafe07c55c083351828c83ddae0
rodrigo <rodrigo.maia.pereira@gmail.com>
2018-02-12 19:10:17 -0200

    availabilities calculation

--- src/availabilities/getAvailabilities.js
+++ src/availabilities/getAvailabilities.js
@@ -1,14 +1,36 @@
-
 import moment from 'moment';
+import { Interval } from 'luxon';
 
 /**
  * getAvailabilities is function that will compute the open time-slots based on officeHours and appointments
  * - once must not assume appointments are
  * @returns {[availabilities]}
  */
+
+const extractInterval = (date) =>  Interval.fromDateTimes(new Date(date.startTime) , new Date(date.endTime));
+const extractIntervals = (dates) =>  Interval.merge(dates.map(extractInterval));
+
 export default function getAvailabilities(officeHours, appointments) {
-  return [
-    { startDate: moment().toISOString(), endDate: moment().toISOString() },
-    { startDate: moment().toISOString(), endDate: moment().toISOString() },
-  ];
+  if (officeHours.isClosed)
+    return [];
+  const breaks = extractIntervals((officeHours.breaks || []).concat(appointments || []));
+  const officeInterval = extractInterval(officeHours);
+  if(breaks.length > 0) {
+    const intervals  = [];
+    breaks.forEach((b, index) => {
+      if( index === 0 && b.start > officeInterval.start ) { // is first break 
+        intervals.push(Interval.fromDateTimes(officeInterval.start, b.start));
+      }
+      if( index + 1 === breaks.length ) { // is last one
+        if(b.end < officeInterval.end) {
+          intervals.push(Interval.fromDateTimes(b.end, officeInterval.end));
+        }
+      } else if (b.end < breaks[index + 1].start) {
+        intervals.push(Interval.fromDateTimes(b.end, breaks[index + 1].start));
+      }
+    });
+    return intervals;
+  }
+
+  return [officeInterval];
 }
--- src/availabilities/getAvailabilities.test.js
+++ src/availabilities/getAvailabilities.test.js
@@ -1,13 +1,92 @@
 
 import getAvailabilities from './getAvailabilities';
 
+// ISO String helper
+const date = (y, m, d, h, mi) => (new Date(y, m, d, h, mi)).toISOString();
+
+
 describe('#getAvailabilities', () => {
   it('is a function', () => {
     expect(typeof getAvailabilities).toBe('function');
   });
 
-  // TODO: remove this and replace with proper testcases
-  it('returns 2 availabilities', () => {
-    expect(getAvailabilities({}, {}).length).toBe(2);
+  it('returns none in closed days', () => {
+    const officeHours =  { isClosed: true };
+
+    const result = getAvailabilities(officeHours, null); 
+
+    expect(result).toEqual([]);
+  });
+
+  it('returns none in closed days', () => {
+    const officeHours =  { isClosed: true };
+
+    const result = getAvailabilities(officeHours, null); 
+
+    expect(result).toEqual([]);
+  });
+
+  it('returns entire day if we do not have appointments or breaks', () => {
+    const officeHours =  {
+      startTime: date(2018, 1, 1, 13, 30), // 1:30pm,
+      endTime: date(2018, 1, 1, 18, 30), // 6:30pm,
+    };
+
+    const intervals = getAvailabilities(officeHours, null); 
+
+    expect(intervals.length).toEqual(1);
+    expect(intervals[0].start.hour).toEqual(13);
+    expect(intervals[0].end.hour).toEqual(18);
+  });
+
+  it('returns entire day without break intervals', () => {
+    const officeHours =  {
+      startTime: date(2018, 1, 1, 8, 0), // 8:00am,
+      endTime: date(2018, 1, 1, 18, 0), // 6:00pm,
+      breaks: [
+        {
+          startTime: date(2018, 1, 1, 12, 0), // 12:00pm,
+          endTime: date(2018, 1, 1, 15, 0), // 3:00pm,
+        },
+      ],
+    };
+
+    const intervals = getAvailabilities(officeHours, null); 
+
+    expect(intervals.length).toEqual(2);
+    expect(intervals[0].start.hour).toEqual(8);
+    expect(intervals[0].end.hour).toEqual(12);
+    expect(intervals[1].start.hour).toEqual(15);
+    expect(intervals[1].end.hour).toEqual(18);
+  });
+
+  it('returns intervals considering break and appointments ', () => {
+    const officeHours =  {
+      startTime: date(2018, 1, 1, 8, 0), // 8:00am,
+      endTime: date(2018, 1, 1, 18, 0), // 6:00pm,
+      breaks: [
+        {
+          startTime: date(2018, 1, 1, 12, 0), // 12:00pm,
+          endTime: date(2018, 1, 1, 15, 0), // 3:00pm,
+        },
+      ],
+    };
+
+    const appointments = [
+      {
+        startTime: date(2018, 1, 1, 16, 0), // 4:00pm,
+        endTime: date(2018, 1, 1, 17, 0), //5:00pm,
+      }
+    ];
+
+    const intervals = getAvailabilities(officeHours, appointments); 
+
+    expect(intervals.length).toEqual(3);
+    expect(intervals[0].start.hour).toEqual(8);
+    expect(intervals[0].end.hour).toEqual(12);
+    expect(intervals[1].start.hour).toEqual(15);
+    expect(intervals[1].end.hour).toEqual(16);
+    expect(intervals[2].start.hour).toEqual(17);
+    expect(intervals[2].end.hour).toEqual(18);
   });
 });

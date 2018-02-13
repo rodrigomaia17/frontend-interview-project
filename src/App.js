39aa34bf87bf4a60ef029176e84968a85362437d
rodrigo <rodrigo.maia.pereira@gmail.com>
2018-02-13 02:20:21 -0200

    link presentational components with getAvailabities function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
 import moment from 'moment';
 import styled from 'styled-components';
+import { DateTime } from 'luxon';
 import logo from './logo.png';
+import { fetchOfficeHours, fetchAppointments } from './fetch/index.js';
+import getAvailabilities from './availabilities/getAvailabilities.js';
 import './App.css';
 
 const Launcher = styled.div`
@@ -22,7 +25,7 @@ const Launcher = styled.div`
 `;
 
 const Window = styled.div`
-width: 370px;
+  width: 370px;
   height: calc(100% - 120px);
   max-height: 590px;
   position: fixed;
@@ -38,13 +41,13 @@ width: 370px;
   border-radius: 10px;
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
 
-@media (max-width: 450px) {
-    width: 100%;
-    height: 100%;
-    max-height: 100%;
-    right: 0px;
-    bottom: 0px;
-    border-radius: 0px;
+  @media (max-width: 450px) {
+  width: 100%;
+  height: 100%;
+  max-height: 100%;
+  right: 0px;
+  bottom: 0px;
+  border-radius: 0px;
   }
 `;
 
@@ -66,39 +69,93 @@ const availabilities = [
   { startDate: moment().toISOString(), endDate: moment().toISOString() },
 ];
 
-const availableItens = availabilities.map((a) => (<li> From: {a.startDate} </li>));
 
-const AppointmentsWindow = ({onClose}) => (
-  <Window>
-    <RoundedDiv className="card" >
-      <header className="card-header">
-        <p className="card-header-title">
-          Appointments
-        </p> 
-        <a href="#" onClick={onClose} className="card-header-icon" aria-label="more options">
-          <span className="icon">
-            <i className="fas fa-times" aria-hidden="true"></i>
-          </span>
-        </a>
-      </header>
-      <DivLeftAligned className="card-content">
-        <div className="field">
-          <label className="label">Select the best day for you:</label>
-          <div className="control">
-            <DayPickerInput />
+class AppointmentsWindow extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedDay: undefined,
+      isLoading: false,
+      slots: []
+    };
+    this.handleDayChange = this.handleDayChange.bind(this);
+  }
+
+  fetchAvailabilities(selectedDay) {
+    Promise.all([fetchOfficeHours(selectedDay), fetchAppointments(selectedDay)])
+           .then((returnValues) => {
+             const officeHours = returnValues[0];
+             const appointments = returnValues[1];
+             const availableIntervals = getAvailabilities(officeHours,appointments);
+             this.setState({slots: availableIntervals, isLoading: false})
+           });
+  }
+
+  handleDayChange(selectedDay) {
+    this.setState({selectedDay, isLoading: true}, () => {
+      this.fetchAvailabilities(selectedDay)
+    });
+  }
+
+  render() {
+    const availableItens = this.state.slots.map((a) => (
+      <li> From {a.start.toLocaleString(DateTime.TIME_SIMPLE)} to {a.end.toLocaleString(DateTime.TIME_SIMPLE)}</li>
+    ));
+
+    return <Window>
+      <RoundedDiv className="card" >
+        <header className="card-header">
+          <p className="card-header-title">
+            Appointments
+          </p> 
+          <a href="#" onClick={this.props.onClose} className="card-header-icon" aria-label="more options">
+            <span className="icon">
+              <i className="fas fa-times" aria-hidden="true"></i>
+            </span>
+          </a>
+        </header>
+        <DivLeftAligned className="card-content">
+          <div className="field">
+            <label className="label">Select the desired day:</label>
+            <div className="control">
+              <DayPickerInput
+                value={this.state.selectedDay}
+                onDayChange={this.handleDayChange}
+              />
+            </div>
           </div>
-        </div>
 
-        <b> Availability Slots:</b>
-        <ul className="menu-list">
-          {availableItens}
-        </ul>
+          { this.state.selectedDay &&
+            (!this.state.isLoading ?
+             <div>
+               <b> Availability Slots:</b>
+               <ul className="menu-list">
+                 {availableItens}
+               </ul>
+             </div>
+            :
+             <LoadingSpinner />)
+          
+          }
+        </DivLeftAligned>
 
-      </DivLeftAligned>
+      </RoundedDiv>
+    </Window>
+  }
+} 
 
-    </RoundedDiv>
-  </Window>
-)
+const SizedDiv = styled.div`
+  height: 350px;
+  text-align: center
+`
+
+const CenterIcon = styled.div`
+  position: relative;
+  top: 50%;
+`
+
+const LoadingSpinner = () => <SizedDiv><CenterIcon> <i className="fa fa-spinner fa-spin fa-4x" > </i></CenterIcon></SizedDiv> ;
 
 class AppointmentsLauncher extends Component {
   constructor() {
